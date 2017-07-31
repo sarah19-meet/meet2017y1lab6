32a99e0f608766f578dc1f079d2467b11d423397
sarah19-meet <sarah19@meet.mit.edu>
2017-07-31 18:30:12 +0300

    Stopping here

--- movingturtle.py
+++ movingturtle.py
@@ -16,25 +16,67 @@ def up():
     direction=UP
     print('you pressed up!')
 
+    
+    turtle.pos()
+    old_pos=turtle.pos()
+    x=old_pos[0]
+    y=old_pos[1]
+    turtle.goto(x,y+10)
+    print(turtle.pos())
+    
+    
+
 
 def left():
     global direction
     direction=LEFT
     print('you pressed left!')
 
+
+    turtle.pos()
+    old_pos=turtle.pos()
+    x=old_pos[0]
+    y=old_pos[1]
+    turtle.goto(x-10,y)
+    print(turtle.pos())
+
 def down():
     global direction
     direction=DOWN
     print('you pressed down!')
+
+
+    turtle.pos()
+    old_pos=turtle.pos()
+    x=old_pos[0]
+    y=old_pos[1]
+    turtle.goto(x,y-10)
+    print(turtle.pos())
+
+    
 
 def right():
     global direction
     direction=RIGHT
     print('you pressed right!')
 
+    turtle.pos()
+    old_pos=turtle.pos()
+    x=old_pos[0]
+    y=old_pos[1]
+    turtle.goto(x+10,y)
+    print(turtle.pos())
+
+
 turtle.onkeypress(up,UP_ARROW)
 turtle.onkeypress(down,DOWN_ARROW)
 turtle.onkeypress(left,LEFT_ARROW)
 turtle.onkeypress(right,RIGHT_ARROW)
+turtle.onkeypress(turtle.stamp,SPACEBAR)
+turtle.listen()
+
+    
+turtle.mainloop()
+
 
     
